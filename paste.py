f849d3439cb81e8bfc8ef22d3f4665dfcd47e761
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-03-04 21:07:12 +0200

    .

---
+++ paste.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import asyncio
+import uiautomator2 as u2
+from ppadb.client import Client as AdbClient
+import re
+import pyperclip
+
+
+d = u2.connect()
+users_output, exit_code = d.shell("pm list users")
+device_profiles = re.findall(r"UserInfo{(\d+):", users_output)
+
+
+async def paste():
+    try:
+        text = pyperclip.paste()
+        print(text)
+
+        d(focused=True).set_text(text)
+    except Exception as error:
+        print(error)
+
+
+async def main():
+    await paste()
+
+
+asyncio.run(main())
